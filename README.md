7106c04fbae263d343afb68610e64dbd57702ce1
razsa <razsagul+uda@gmail.com>
2017-09-02 12:46:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Movie Trailer Website
+
+This is a movie trailer website that lists movies, with box art and a trailer to each movie.
+
+# Requirements
+
+Requires: Python version 2.7.x
+
+# Get Started
+
+- Run via Terminal:
+Step 1: Navigate to the `src` folder 
+Step 2: Type python `entertainment_center.py`
+
+- Run via Python IDLE:
+Click `Run Module` from the dropdown menu
+
+- Run via Browser:
+Double-Click the `fresh_tomatoes.html` to view in your favorite browser.
---
+++ src/entertainment_center.py
@@ -0,0 +1,25 @@
+import fresh_tomatoes
+import media
+
+# This opens the fresh tomatoes application and displays these movies
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://goo.gl/T4dPBt",
+                        "https://www.youtube.com/watch?v=Ny_hRfvsmU8")
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://goo.gl/HRZL6G",
+                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
+
+the_matrix = media.Movie("The Matrix",
+                         "A hacker saves humanity from AI machines.",
+                         "https://goo.gl/g2RdoL",
+                         "https://www.youtube.com/watch?v=vKQi3bBA1y8")
+
+movies = [toy_story, avatar, the_matrix]
+
+# This function call opens a page of movies generated from the input, an list
+# of movie instances.
+fresh_tomatoes.open_movies_page(movies)
